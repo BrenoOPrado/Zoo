60c669152a9db3aa1ef58ce48c1714740b55903a
Breno Prado <breno.o.prado@hotmail.com>
2022-05-20 15:13:54 -0300

    Requisito 2 completo :turtle:

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animalObj = Object.values(data.species).find((obj) => obj.name === animal);
+  return animalObj.residents.every((residentAge) => residentAge.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
