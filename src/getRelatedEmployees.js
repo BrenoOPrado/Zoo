a43d0df8bd4a1f75fa43c976f15d0afe04ae6e80
Breno Prado <breno.o.prado@hotmail.com>
2022-05-20 18:06:43 -0300

    Requisito 4 completo :turtle:

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const obj = Object.values(data.employees).find((person) => person.id === id);
+  if (obj.managers.length < 2) {
+    return true;
+  }
+  return false;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const arr = [];
+  if (isManager(managerId)) {
+    const filter = (obj) => obj.managers.find((id) => id === managerId);
+    const managerArr = Object.values(data.employees).filter(filter);
+    managerArr.forEach((element) => arr.push(`${element.firstName} ${element.lastName}`));
+  } else {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return arr;
 }
 
 module.exports = { isManager, getRelatedEmployees };
