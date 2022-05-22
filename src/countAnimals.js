56b3460c2bc3a6f675201c26f614495b846aacf1
Breno Prado <breno.o.prado@hotmail.com>
2022-05-22 12:06:45 -0300

    Requisito 5 completo :turtle:

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal !== undefined) {
+    const animalObj = data.species.find((item) => item.name === Object.values(animal)[0]);
+    if (Object.values(animal).length > 1) {
+      const filterSex = animalObj.residents.filter((item) => item.sex === Object.values(animal)[1]);
+      return filterSex.length;
+    }
+    return animalObj.residents.length;
+  }
+  const generalResult = {};
+  data.species.forEach((item) => {
+    generalResult[item.name] = item.residents.length;
+  });
+  return generalResult;
 }
 
 module.exports = countAnimals;
