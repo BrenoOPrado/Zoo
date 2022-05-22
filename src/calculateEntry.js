491e050e874975a0cb4666e8737a620804e5b012
Breno Prado <breno.o.prado@hotmail.com>
2022-05-22 12:57:19 -0300

    Requisito 8 completo :turtle:

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,28 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const people = { adult: 0, child: 0, senior: 0 };
+  entrants.forEach((element) => {
+    if (element.age < 18) {
+      people.child += 1;
+    } else if (element.age < 50) {
+      people.adult += 1;
+    } else if (element.age >= 50) {
+      people.senior += 1;
+    }
+  });
+  return people;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.values(entrants).length === 0) {
+    return 0;
+  }
+  const people = countEntrants(entrants);
+  const adults = Object.values(people)[0] * data.prices.adult;
+  const child = Object.values(people)[1] * data.prices.child;
+  const senior = Object.values(people)[2] * data.prices.senior;
+  return adults + child + senior;
 }
 
 module.exports = { calculateEntry, countEntrants };
