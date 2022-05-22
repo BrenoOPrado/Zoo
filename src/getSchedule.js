81ec029785dc8a983d9daaca178f925af6d807b5
Breno Prado <breno.o.prado@hotmail.com>
2022-05-22 15:49:14 -0300

    inicio de reformulação para realizar a etapa 9 :turtle:

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,27 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
+function getScheduleDay(day) {
+
 }
+function getScheduleAnimal(animal) {
+
+}
+function getScheduleGeneral(general) {
+
+}
+
+function getSchedule(scheduleTarget) {
+  const daySome = Object.keys(data.hours).some((item) => item === scheduleTarget);
+  const animalSome = Object.values(data.species).some((item) => item.name === scheduleTarget);
+  if (daySome) {
+    return getScheduleDay(scheduleTarget);
+  }
+  if (animalSome) {
+    return getScheduleAnimal(scheduleTarget);
+  }
+  return getScheduleGeneral(scheduleTarget);
+}
+
+console.log(getSchedule('Tuesday'));
 
 module.exports = getSchedule;
