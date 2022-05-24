cb8a0e5716b188d61003b8fc900aafd7d38f6506
Breno Prado <breno.o.prado@hotmail.com>
2022-05-24 16:05:34 -0300

    Requisito 13 e 15 realizado :turtle:

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -17,9 +17,12 @@ describe('Testes da função getOpeningHours', () => {
     expect(getOpeningHours('Tuesday', '07:00-PM')).toBe(close);
     expect(getOpeningHours('Monday', '09:00-AM')).toBe(close);
     expect(getOpeningHours('Wednesday', '09:00-PM')).toBe(close);
-    expect(getOpeningHours('Thu', '09:00-PM')).toThrowError(new Error('The day must be valid. Example: Monday'));
-    expect(getOpeningHours('Friday', '09:00-ZM')).toThrowError(new Error('The abbreviation must be \'AM\' or \'PM\''));
-    expect(getOpeningHours('Friday', 'C9:00-AM')).toThrowError(new Error('The hour should represent a number'));
-    expect(getOpeningHours('Friday', '09:c0-AM')).toThrowError(new Error('The minutes should represent a number'));
+
+    expect(() => getOpeningHours('Thu', '09:00-PM')).toThrow('The day must be valid. Example: Monday');
+    expect(() => getOpeningHours('Friday', '09:00-ZM')).toThrow('The abbreviation must be \'AM\' or \'PM\'');
+    expect(() => getOpeningHours('Friday', 'C9:00-AM')).toThrow('The hour should represent a number');
+    expect(() => getOpeningHours('Friday', '09:c0-AM')).toThrow('The minutes should represent a number');
+    expect(() => getOpeningHours('Friday', '09:70-AM')).toThrow('The minutes must be between 0 and 59');
+    expect(() => getOpeningHours('Friday', '13:00-AM')).toThrow('The hour must be between 0 and 12');
   });
 });
