a55632b0b2c0e87ee8d96896fee7939fcd7c063d
Breno Prado <breno.o.prado@hotmail.com>
2022-05-25 18:21:11 -0300

    Tentativa 1 do requisito 14 (nesse me recuso a pedir ajuda, vou conseguir batendo a cabeça mesmo) :turtle:

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,64 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+function animalMap(options) {
+  const result = {};
+  data.species.forEach((obj) => {
+    if (!Object.keys(result).some((key) => key === obj.location)) {
+      const filterAnimals = data.species.filter((loc) => loc.location === obj.location);
+      const filterAnimalsMap = filterAnimals.map((animals) => animals.name);
+      result[obj.location] = filterAnimalsMap;
+    }
+  });
+  return result;
 }
+
+function includeName(element, sex) {
+  const elementObj = data.species.find((obj) => obj.name === element);
+  const namesObj = [];
+  if (sex === undefined) {
+    elementObj.residents.forEach((resident) => {
+      namesObj.push(resident.name);
+    });
+  } else {
+    const elementObjFilter = elementObj.residents.filter((item) => item.sex === sex);
+    elementObjFilter.forEach((resident) => {
+      namesObj.push(resident.name);
+    });
+  }
+  return namesObj;
+}
+
+function animalMapIncludeName(generalMap, sex, sort) {
+  const namesArr = [];
+  Object.keys(generalMap).forEach((key) => {
+    generalMap[key].forEach((element, index) => {
+      const names = includeName(element, sex);
+      if (sort !== undefined) {
+        names.sort();
+      }
+      namesArr.push(names);
+      console.log(namesArr);
+    });
+  });
+}
+
+function getAnimalMap(options) {
+  const param = [];
+  if (options !== undefined) {
+    if (Object.keys(options).some((key) => key === 'includeNames')) {
+      param.push(animalMap(options));
+    }
+    if (Object.keys(options).some((key) => key === 'sex')) {
+      param.push(options.sex);
+    }
+    if (Object.keys(options).some((key) => key === 'sorted')) {
+      param.push(options.sorted);
+    }
+    return animalMapIncludeName(...param);
+  }
+  return animalMap(options);
+}
+
+console.log(getAnimalMap({ includeNames: true, sex: 'female' }));
 
 module.exports = getAnimalMap;
